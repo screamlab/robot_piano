1084c474b67d2af66e39005db844ffe47774add8
Otis Chung <p76124605@gs.ncku.edu.tw>
2025-04-02 15:51:03 +0800

    [FEAT] Add concurrently services call

--- scripts/trigger.sh
+++ scripts/trigger.sh
@@ -1,3 +1,19 @@
 #!/bin/bash
 
-ros2 service call /start_sync std_srvs/srv/Trigger
+# Function to handle SIGINT (Ctrl-C)
+cleanup() {
+    echo "Received SIGINT, stopping background processes..."
+    # Kill all background jobs started by this script
+    kill -9 $(jobs -p)
+    exit 0
+}
+
+# Set up trap for SIGINT
+trap cleanup SIGINT
+
+# Run both commands concurrently in the background
+ros2 service call /left_start_sync std_srvs/srv/Trigger &
+ros2 service call /right_start_sync std_srvs/srv/Trigger &
+
+# Wait for both background processes to finish
+wait
