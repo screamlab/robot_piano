c4687004acb42dd85a1e879812eef7c04cc3b7b7
Otis Chung <p76124605@gs.ncku.edu.tw>
2025-04-15 18:39:53 +0800

    [FEAT] Use rclcpp trigger in shell script

--- scripts/trigger.sh
+++ scripts/trigger.sh
@@ -12,8 +12,9 @@ cleanup() {
 trap cleanup SIGINT
 
 # Run both commands concurrently in the background
-ros2 service call /left_start_sync std_srvs/srv/Trigger &
-ros2 service call /right_start_sync std_srvs/srv/Trigger &
+# ros2 service call /left_start_sync std_srvs/srv/Trigger &
+# ros2 service call /right_start_sync std_srvs/srv/Trigger &
+docker exec -it moveit2_container_left bash -c "source ./install/setup.bash && ros2 run robot_piano trigger"
 
 # Wait for both background processes to finish
 wait
